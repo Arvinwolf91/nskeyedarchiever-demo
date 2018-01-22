aca85d5c5b143d003b7aef8e0bbbd8323bd6d086
arvin.sanmuga <arvin_wolf@Arvins-MacBook-Pro-2.local>
2018-01-22 14:16:59 +0800

    [] Initial commit

---
+++ NSKeyedArchiverDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSKeyedArchiverDemo
+//
+//  Created by Arvin Sanmuga Rajah on 22/01/2018.
+//  Copyright © 2018 Arvin Sanmuga Rajah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
